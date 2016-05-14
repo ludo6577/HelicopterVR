26e4bc2f86fcf83de44ac14ddcf11f491c005d92
MattGen <magenoud@gmail.com>
2016-05-14 06:58:44 +0200

    tERRAIN TEST +minimap

---
+++ Assets/Editor/$PlaceTreeShadowCasters.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEditor;
+
+public class PlaceTreeShadowCasters 
+{
+	
+	[@MenuItem ("Terrain/Place Tree Shadow Casters")]
+	static void Run()
+	{
+		Terrain terrain = Terrain.activeTerrain;
+		if (terrain == null ) 
+		{
+			return;
+		}
+		
+		TerrainData td = terrain.terrainData;
+		
+		GameObject parent = new GameObject("Tree Shadow Casters");
+		foreach (TreeInstance tree in td.treeInstances) 
+		{
+			Vector3 pos = Vector3.Scale(tree.position, td.size) + terrain.transform.position;
+			TreePrototype treeProt = td.treePrototypes[tree.prototypeIndex];
+			GameObject prefab = treeProt.prefab;
+
+			Debug.Log ("tree : " + tree.rotation);
+			GameObject obj = GameObject.Instantiate(prefab, pos, Quaternion.AngleAxis(tree.rotation * Mathf.Rad2Deg, Vector3.up)) as GameObject;
+			MeshRenderer renderer = obj.GetComponentInChildren<MeshRenderer>();
+			renderer.receiveShadows = false;
+			renderer.shadowCastingMode = ShadowCastingMode.On;
+			GameObjectUtility.SetStaticEditorFlags(obj, StaticEditorFlags.LightmapStatic);
+			
+			Transform t = obj.transform;
+			t.localScale = new Vector3(tree.widthScale, tree.heightScale, tree.widthScale);
+			t.rotation = Quaternion.AngleAxis (tree.rotation * Mathf.Rad2Deg, Vector3.up);
+			t.parent = parent.transform;
+		}
+	}  
+}
