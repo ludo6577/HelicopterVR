73e80fb9e8d9de4a60bad31b88a2a504d67d4b32
ludo6577 <ludo6577@gmail.com>
2016-05-16 21:59:16 +0200

    Reordering

---
+++ Assets/Scripts/Checkpoint/CheckpointController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Security.Policy;
+
+public class CheckpointController : MonoBehaviour
+{
+    public Checkpoint[] CheckpointsList;
+
+    private Checkpoint CurrentCheckpoint;
+    private int CheckpointId;
+
+	// Use this for initialization
+	void Start ()
+	{
+        if (CheckpointsList.Length==0) return;
+
+	    for (int index = 0; index < CheckpointsList.Length; index++)
+            CheckpointsList[index].gameObject.SetActive(false);
+
+	    CheckpointId = 0;
+        SetCurrentCheckpoint(CheckpointsList[CheckpointId]);
+	}
+
+    private void SetCurrentCheckpoint(Checkpoint checkpoint)
+    {
+        if (CurrentCheckpoint != null)
+        {
+            CurrentCheckpoint.gameObject.SetActive(false);
+            CurrentCheckpoint.CheckpointActivated -= CheckpointActivated;
+        }
+
+        CurrentCheckpoint = checkpoint;
+        CurrentCheckpoint.CheckpointActivated += CheckpointActivated;
+        CurrentCheckpoint.gameObject.SetActive(true);
+    }
+
+    private void CheckpointActivated()
+    {
+        CheckpointId++;
+        if (CheckpointId >= CheckpointsList.Length)
+        {
+            CurrentCheckpoint.gameObject.SetActive(false);
+            CurrentCheckpoint.CheckpointActivated -= CheckpointActivated;
+            return;
+        }
+
+        SetCurrentCheckpoint(CheckpointsList[CheckpointId]);
+    }
+
+    // Update is called once per frame
+	void Update () {
+	
+	}
+}
